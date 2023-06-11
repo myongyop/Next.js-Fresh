e1f6625e3a857b944318d07d915af90d4d591188
tanberine <mylee@tanberine.com>
2023-06-11 21:29:21 +0900

    props 테스트

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,21 +1,29 @@
 import {age, name} from './data.js';
 
 export default function Cart() {
+  let 장바구니 = ['Tomatoes', 'Pasta', 'Apple'];
+
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <CartItem />
-      <CartItem />
-      <CartItem />
+      <CartItem item={장바구니[0]}/>
+      <CartItem item={장바구니[1]}/>
+      <CartItem item={장바구니[2]}/>
+      <Banner content="롯데카드" />
+      <Banner content="현대카드" />
     </div>
   )
 }
 
-function CartItem(){
+function Banner(props){
+  return <h5>{props.content} 결제 행사중 </h5>
+}
+
+function CartItem(props){
   return (
     <div className="cart-item">
-    <p>상품ss명 {age}</p>
-    <p>$40 {name}</p>
+    <p>{props.item}</p>
+    <p>$40</p>
     <p>1개</p>
   </div>
   )
