d191972398cda0f4f1f17261e2e18ac4f59dce60
tanberine <mylee@tanberine.com>
2023-06-11 15:09:07 +0900

    React 기초 테스트

--- app/page.js
+++ app/page.js
@@ -2,9 +2,15 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+  let name = 'zend'
+  let age = 20
+  let link = 'http://googlec.com'
   return (
     <div>
-      <h4 className='red'>Next App</h4>
+      <h4 className='title'>애플후레시 {name}</h4>
+      <p className='title-sub'>by zend {age}</p>
+      <a href="{link}">링크</a>
+      <p style={{ color:'red' }}>by zend {age}</p>
     </div>
   )
 }
