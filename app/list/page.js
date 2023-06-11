a1be96366ace492ad9cb5e8b95038a9a4d31869c
tanberine <mylee@tanberine.com>
2023-06-11 16:01:02 +0900

    Map 배열 가져오기

--- app/list/page.js
+++ app/list/page.js
@@ -1,14 +1,34 @@
 export default function List() {
+  let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+  let a = [20, 30, 40]
+  let AAAA = [2,3,4]
+  let b = AAAA.map((a, i) => {
+    return 10
+  })
+
+  console.log(b)
+
+  console.log(a[0])
+  console.log(a[1])
+  console.log(a[2])
+
+  a[0] = 100
+
+  console.log(a)
 
   return (
     <div>
       <h4 className='title'>상품목록</h4>
-      <div className="food">
-        <h4>상품1 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품2 $40</h4>
-      </div>
+      {
+        상품.map((a, i) => {
+          return (
+            <div className="food">
+            <h4>{상품[i]} $40</h4>
+            </div>
+          )
+          
+        })
+      }
     </div>
   )
 }
